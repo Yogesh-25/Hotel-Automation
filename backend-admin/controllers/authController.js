b7ca454cd5165e65d5923b9ff80287835d5e9086
Yogesh-25 <103736965+Yogesh-25@users.noreply.github.com>
2024-06-11 17:24:19 +0530

    updated code

--- backend-admin/controllers/authController.js
+++ backend-admin/controllers/authController.js
@@ -1,33 +1,25 @@
 const User = require('../models/User');
 const sendEmail = require('../utils/sendEmail');
-const jwt = require('jsonwebtoken');
 const randomstring = require('randomstring');
-
-const secretKey = 'dfad07d0558a3a33cf9cc0960280cd540316a456745e8458ab9c879f9331f71a';
 
 exports.verifyOtp = async (req, res) => {
     const { email, otp } = req.body;
-
     try {
         const user = await User.findOne({ email });
-
         if (!user) {
             return res.status(404).json({ success: false, message: 'User not found' });
         }
-
         if (user.resetPasswordOTP !== otp) {
             return res.status(400).json({ success: false, message: 'Invalid OTP' });
         }
-
         // Check if OTP has expired
         const otpExpiration = user.resetPasswordOTPExpiration;
         if (otpExpiration && otpExpiration < Date.now()) {
             return res.status(400).json({ success: false, message: 'OTP has expired' });
         }
-
         // OTP verified successfully
-        user.resetPasswordOTP = ''; // Clear the OTP after verification
-        user.resetPasswordOTPExpiration = null; // Clear the expiration time
+        user.resetPasswordOTP = '';
+        user.resetPasswordOTPExpiration = null;
         await user.save();
 
         res.status(200).json({ success: true, message: 'OTP verified successfully' });
@@ -43,14 +35,8 @@ exports.forgotPassword = async (req, res) => {
     if (!user) {
         return res.status(404).json({ message: 'User not found' });
     }
-
-    // Generate random 5-digit OTP
     const otp = randomstring.generate({ length: 5, charset: 'numeric' });
-
-    // Set OTP expiration time (5 minutes)
     const otpExpiration = Date.now() + 5 * 60 * 1000;
-
-    // Email template with OTP
     const message = `Your OTP for password reset is: ${otp}`;
 
     try {
@@ -59,8 +45,6 @@ exports.forgotPassword = async (req, res) => {
             subject: 'Password Reset OTP',
             text: message,
         });
-
-        // Store OTP and its expiration time in user document (for verification later)
         user.resetPasswordOTP = otp;
         user.resetPasswordOTPExpiration = otpExpiration;
         await user.save();
@@ -77,10 +61,7 @@ exports.resetPassword = async (req, res) => {
     const { email } = req.params;
 
     try {
-        // Find user by email
         const user = await User.findOne({ email });
-
-        // Update user's password
         user.password = password;
         await user.save();
 
